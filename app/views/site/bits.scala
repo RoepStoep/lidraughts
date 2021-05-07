9acca097ecd66ed669f5a526b5bd15208a56b747
Niklas Fiekas <niklas.fiekas@backscattering.de>
2021-05-07 10:58:57 +0200

    no need for google fonts on /api (#8701)

--- app/views/site/bits.scala
+++ app/views/site/bits.scala
@@ -26,10 +26,9 @@ object bits {
 <html>
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src https://fonts.googleapis.com 'unsafe-inline'; font-src https://fonts.gstatic.com; script-src 'unsafe-eval' https://cdn.jsdelivr.net blob:; child-src blob:; connect-src https://raw.githubusercontent.com; img-src data: https://lidraughts.org;">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'unsafe-inline'; script-src 'unsafe-eval' https://cdn.jsdelivr.net blob:; child-src blob:; connect-src https://raw.githubusercontent.com; img-src data: https://lidraughts.org;">
     <title>Lidraughts.org API reference</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Roboto:300,400,700" rel="stylesheet">
     <link rel="icon" type="image/png" href="https://lidraughts.org/assets/images/favicon-32-white.png">
     <style>body { margin: 0; padding: 0; }</style>
   </head>
