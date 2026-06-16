lichess-org/lila

team-featured swiss WIP

modules/swiss/src/main/SwissFeature.scala
@@ -1,25 +1,53 @@
 package lila.swiss
 
 import org.joda.time.DateTime
+import reactivemongo.api.ReadPreference
 import scala.concurrent.duration._
 
+import lila.common.Heapsort
 import lila.db.dsl._
+import lila.hub.LightTeam.TeamID
 import lila.memo.CacheApi
 import lila.memo.CacheApi._
 
 final class SwissFeature(
     colls: SwissColls,
-    cacheApi: CacheApi
+    cacheApi: CacheApi,
+    swissCache: SwissCache
 )(implicit ec: scala.concurrent.ExecutionContext) {
 
   import BsonHandlers._
 
+  def get(teams: Seq[TeamID]) =
+    cache.getUnit zip getForTeams(teams) map {
+      case (cached, teamed) =>
+        FeaturedSwisses(
+          created = teamed.created ::: cached.created,
+          started = teamed.started ::: cached.started
+        )
+    }
+
+  private val startsAtOrdering = Ordering.by[Swiss, Long](_.startsAt.getMillis)
+
+  private def getForTeams(teams: Seq[TeamID]): Fu[FeaturedSwisses] =
+    teams.map(swissCache.featuredInTeam.get).sequenceFu.map(_.flatten) flatMap { ids =>
+      colls.swiss.byIds[Swiss](ids.map(_.value), ReadPreference.secondaryPreferred)
+    } map {
+      _.filter(_.isNotFinished).partition(_.isCreated) match {
+        case (created, started) =>
+          FeaturedSwisses(
+            created = Heapsort.topN(created, 10, startsAtOrdering),
+            started = Heapsort.topN(started, 10, startsAtOrdering.reverse)
+          )
+      }
+    }
+
   private val cache = cacheApi.unit[FeaturedSwisses] {
     _.refreshAfterWrite(10 seconds)
       .buildAsyncFuture { _ =>
-        cacheCompute($doc("$lt" -> DateTime.now)) zip
-          cacheCompute($doc("$gt" -> DateTime.now, "$lt" -> DateTime.now.plusHours(1))) map {
-          case (a, b) => FeaturedSwisses(a, b)
+        cacheCompute($doc("$gt" -> DateTime.now, "$lt" -> DateTime.now.plusHours(1))) zip
+          cacheCompute($doc("$lt" -> DateTime.now)) map {
+          case (created, started) => FeaturedSwisses(created, started)
         }
       }
   }
@@ -36,6 +64,4 @@ final class SwissFeature(
       .sort($sort desc "nbPlayers")
       .cursor[Swiss]()
       .list(5)
-
-  def get = cache.getUnit
 }

RoepStoep/lidraughts

modules/swiss/src/main/SwissFeature.scala
@@ -1,23 +1,51 @@
 package lidraughts.swiss
 
 import org.joda.time.DateTime
+import reactivemongo.api.ReadPreference
 import scala.concurrent.duration._
 
+import lidraughts.common.Heapsort
 import lidraughts.db.dsl._
+import lidraughts.hub.lightTeam.TeamId
 import lidraughts.memo._
 
 final class SwissFeature(
     swissColl: Coll,
-    asyncCache: lidraughts.memo.AsyncCache.Builder
+    asyncCache: lidraughts.memo.AsyncCache.Builder,
+    swissCache: SwissCache
 ) {
 
   import BsonHandlers._
 
+  def get(teams: Seq[TeamId]) =
+    cache.get zip getForTeams(teams) map {
+      case (cached, teamed) =>
+        FeaturedSwisses(
+          created = teamed.created ::: cached.created,
+          started = teamed.started ::: cached.started
+        )
+    }
+
+  private val startsAtOrdering = Ordering.by[Swiss, Long](_.startsAt.getMillis)
+
+  private def getForTeams(teams: Seq[TeamId]): Fu[FeaturedSwisses] =
+    teams.map(swissCache.featuredInTeam.get).sequenceFu.map(_.flatten) flatMap { ids =>
+      swissColl.byIds[Swiss](ids.map(_.value), ReadPreference.secondaryPreferred)
+    } map {
+      _.filter(_.isNotFinished).partition(_.isCreated) match {
+        case (created, started) =>
+          FeaturedSwisses(
+            created = Heapsort.topN(created, 10, startsAtOrdering),
+            started = Heapsort.topN(started, 10, startsAtOrdering.reverse)
+          )
+      }
+    }
+
   private val cache = asyncCache.single[FeaturedSwisses](
     name = "swiss.featurable",
-    f = cacheCompute($doc("$lt" -> DateTime.now)) zip
-      cacheCompute($doc("$gt" -> DateTime.now, "$lt" -> DateTime.now.plusHours(1))) map {
-        case (a, b) => FeaturedSwisses(a, b)
+    f = cacheCompute($doc("$gt" -> DateTime.now, "$lt" -> DateTime.now.plusHours(1))) zip
+      cacheCompute($doc("$lt" -> DateTime.now)) map {
+        case (created, started) => FeaturedSwisses(created, started)
       },
     expireAfter = _.ExpireAfterWrite(10 seconds)
   )
@@ -33,6 +61,4 @@ final class SwissFeature(
       )
       .sort($sort desc "nbPlayers")
       .list[Swiss](5)
-
-  def get = cache.get
 }