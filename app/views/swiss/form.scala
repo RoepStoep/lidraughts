lichess-org/lila

swiss entry conditions - closes #6657

app/views/swiss/form.scala
@@ -1,14 +1,14 @@
 package views.html.swiss
 
+import controllers.routes
 import play.api.data.Form
 
-import controllers.routes
 import lila.api.Context
 import lila.app.templating.Environment._
-import lila.tournament.TournamentForm
-import lila.swiss.{ Swiss, SwissForm }
 import lila.app.ui.ScalatagsTemplate._
 import lila.hub.LightTeam.TeamID
+import lila.swiss.{ Swiss, SwissCondition, SwissForm }
+import lila.tournament.TournamentForm
 
 object form {
 
@@ -35,6 +35,7 @@ object form {
               fields.chatFor,
               fields.password
             ),
+            condition(form, fields, swiss = none),
             form3.globalError(form),
             form3.actions(
               a(href := routes.Team.show(teamId))(trans.cancel()),
@@ -68,6 +69,7 @@ object form {
               fields.chatFor,
               fields.password
             ),
+            condition(form, fields, swiss = swiss.some),
             form3.globalError(form),
             form3.actions(
               a(href := routes.Swiss.show(swiss.id.value))(trans.cancel()),
@@ -82,6 +84,31 @@ object form {
         )
       )
     }
+
+  private def condition(form: Form[_], fields: SwissFields, swiss: Option[Swiss])(implicit ctx: Context) =
+    frag(
+      form3.split(
+        form3.group(form("conditions.nbRatedGame.nb"), frag("Minimum rated games"), half = true)(
+          form3.select(_, SwissCondition.DataForm.nbRatedGameChoices)
+        ),
+        (ctx.me.exists(_.hasTitle) || isGranted(_.ManageTournament)) ?? {
+          form3.checkbox(
+            form("conditions.titled"),
+            frag("Only titled players"),
+            help = frag("Require an official title to join the tournament").some,
+            half = true
+          )
+        }
+      ),
+      form3.split(
+        form3.group(form("conditions.minRating.rating"), frag("Minimum rating"), half = true)(
+          form3.select(_, SwissCondition.DataForm.minRatingChoices)
+        ),
+        form3.group(form("conditions.maxRating.rating"), frag("Maximum weekly rating"), half = true)(
+          form3.select(_, SwissCondition.DataForm.maxRatingChoices)
+        )
+      )
+    )
 }
 
 final private class SwissFields(form: Form[_])(implicit ctx: Context) {

app/views/swiss/show.scala
@@ -1,20 +1,20 @@
 package views.html
 package swiss
 
+import controllers.routes
 import play.api.libs.json.Json
 
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
 import lila.common.String.html.safeJsonValue
-import lila.swiss.Swiss
-
-import controllers.routes
+import lila.swiss.{ Swiss, SwissCondition }
 
 object show {
 
   def apply(
       s: Swiss,
+      verdicts: SwissCondition.All.WithVerdicts,
       data: play.api.libs.json.JsObject,
       chatOption: Option[lila.chat.UserChat.Mine],
       isLocalMod: Boolean
@@ -66,7 +66,7 @@ object show {
     )(
       main(cls := "swiss")(
         st.aside(cls := "swiss__side")(
-          swiss.side(s, chatOption.isDefined)
+          swiss.side(s, verdicts, chatOption.isDefined)
         ),
         div(cls := "swiss__main")(div(cls := "box"))
       )

app/views/swiss/side.scala
@@ -1,19 +1,21 @@
 package views
 package html.swiss
 
+import controllers.routes
+
 import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
 import lila.common.String.html.markdownLinksOrRichText
-import lila.swiss.Swiss
-
-import controllers.routes
+import lila.swiss.{ Swiss, SwissCondition }
 
 object side {
 
   private val separator = " • "
 
-  def apply(s: Swiss, chat: Boolean)(implicit ctx: Context) =
+  def apply(s: Swiss, verdicts: SwissCondition.All.WithVerdicts, chat: Boolean)(implicit
+      ctx: Context
+  ) =
     frag(
       div(cls := "swiss__meta")(
         st.section(dataIcon := s.perfType.map(_.iconChar.toString))(
@@ -49,7 +51,29 @@ object side {
         },
         s.looksLikePrize option views.html.tournament.bits.userPrizeDisclaimer(s.createdBy),
         teamLink(s.teamId),
-        separator,
+        if (verdicts.relevant)
+          st.section(
+            dataIcon := "7",
+            cls := List(
+              "conditions" -> true,
+              "accepted"   -> (ctx.isAuth && verdicts.accepted),
+              "refused"    -> (ctx.isAuth && !verdicts.accepted)
+            )
+          )(
+            div(
+              verdicts.list.sizeIs < 2 option p(trans.conditionOfEntry()),
+              verdicts.list map { v =>
+                p(
+                  cls := List(
+                    "condition text" -> true,
+                    "accepted"       -> v.verdict.accepted,
+                    "refused"        -> !v.verdict.accepted
+                  )
+                )(s.perfType map v.condition.name)
+              }
+            )
+          )
+        else br,
         absClientDateTime(s.startsAt)
       ),
       chat option views.html.chat.frag

RoepStoep/lidraughts

app/views/swiss/form.scala
@@ -1,14 +1,14 @@
 package views.html.swiss
 
+import controllers.routes
 import play.api.data.Form
 
-import controllers.routes
 import lidraughts.api.Context
 import lidraughts.app.templating.Environment._
-import lidraughts.tournament.{ DataForm => TourForm }
-import lidraughts.swiss.{ Swiss, SwissForm }
 import lidraughts.app.ui.ScalatagsTemplate._
 import lidraughts.hub.lightTeam.TeamId
+import lidraughts.swiss.{ Swiss, SwissCondition, SwissForm }
+import lidraughts.tournament.{ DataForm => TourForm }
 
 object form {
 
@@ -34,6 +34,7 @@ object form {
                 fields.roundInterval,
                 fields.startsAt
               ),
+              condition(form, fields, swiss = none),
               form3.globalError(form),
               form3.actions(
                 a(href := routes.Team.show(teamId))(trans.cancel()),
@@ -66,6 +67,7 @@ object form {
                 fields.roundInterval,
                 swiss.isCreated option fields.startsAt
               ),
+              condition(form, fields, swiss = none),
               form3.globalError(form),
               form3.actions(
                 a(href := routes.Swiss.show(swiss.id.value))(trans.cancel()),
@@ -80,6 +82,31 @@ object form {
           )
         )
       }
+
+  private def condition(form: Form[_], fields: SwissFields, swiss: Option[Swiss])(implicit ctx: Context) =
+    frag(
+      form3.split(
+        form3.group(form("conditions.nbRatedGame.nb"), trans.minimumRatedGames(), half = true)(
+          form3.select(_, SwissCondition.DataForm.nbRatedGameChoices)
+        ),
+        (ctx.me.exists(_.hasTitle) || isGranted(_.ManageTournament)) ?? {
+          form3.checkbox(
+            form("conditions.titled"),
+            trans.onlyTitled(),
+            help = trans.onlyTitledHelp().some,
+            half = true
+          )
+        }
+      ),
+      form3.split(
+        form3.group(form("conditions.minRating.rating"), trans.minimumRating(), half = true)(
+          form3.select(_, SwissCondition.DataForm.minRatingChoices)
+        ),
+        form3.group(form("conditions.maxRating.rating"), trans.maximumWeeklyRating(), half = true)(
+          form3.select(_, SwissCondition.DataForm.maxRatingChoices)
+        )
+      )
+    )
 }
 
 final private class SwissFields(form: Form[_])(implicit ctx: Context) {

app/views/swiss/show.scala
@@ -1,20 +1,20 @@
 package views.html
 package swiss
 
+import controllers.routes
 import play.api.libs.json.Json
 
 import lidraughts.api.Context
 import lidraughts.app.templating.Environment._
 import lidraughts.app.ui.ScalatagsTemplate._
 import lidraughts.common.String.html.safeJsonValue
-import lidraughts.swiss.Swiss
-
-import controllers.routes
+import lidraughts.swiss.{ Swiss, SwissCondition }
 
 object show {
 
   def apply(
     s: Swiss,
+    verdicts: SwissCondition.All.WithVerdicts,
     data: play.api.libs.json.JsObject,
     chatOption: Option[lidraughts.chat.UserChat.Mine]
   )(implicit ctx: Context): Frag = {
@@ -65,7 +65,7 @@ object show {
     )(
         main(cls := "swiss")(
           st.aside(cls := "swiss__side")(
-            swiss.side(s, chatOption.isDefined)
+            swiss.side(s, verdicts, chatOption.isDefined)
           ),
           div(cls := "swiss__main")(div(cls := "box"))
         )

app/views/swiss/side.scala
@@ -1,20 +1,20 @@
 package views
 package html.swiss
 
+import controllers.routes
+
 import lidraughts.api.Context
 import lidraughts.app.templating.Environment._
 import lidraughts.app.ui.ScalatagsTemplate._
 import lidraughts.common.String.html.markdownLinksOrRichText
 import lidraughts.common.String.html.richText
-import lidraughts.swiss.Swiss
-
-import controllers.routes
+import lidraughts.swiss.{ Swiss, SwissCondition }
 
 object side {
 
   private val separator = " • "
 
-  def apply(s: Swiss, chat: Boolean)(implicit ctx: Context) = frag(
+  def apply(s: Swiss, verdicts: SwissCondition.All.WithVerdicts, chat: Boolean)(implicit ctx: Context) = frag(
     div(cls := "swiss__meta")(
       st.section(dataIcon := s.perfType.map(_.iconChar.toString))(
         div(
@@ -47,7 +47,25 @@ object side {
         st.section(cls := "description")(markdownLinksOrRichText(d))
       },
       teamLink(s.teamId),
-      separator,
+      if (verdicts.relevant) st.section(dataIcon := "7", cls := List(
+        "conditions" -> true,
+        "accepted" -> (ctx.isAuth && verdicts.accepted),
+        "refused" -> (ctx.isAuth && !verdicts.accepted)
+      ))(
+        div(
+          (verdicts.list.size < 2) option p(trans.conditionOfEntry()),
+          verdicts.list map { v =>
+            p(
+              cls := List(
+                "condition text" -> true,
+                "accepted" -> v.verdict.accepted,
+                "refused" -> !v.verdict.accepted
+              )
+            )(s.perfType map v.condition.name)
+          }
+        )
+      )
+      else br,
       absClientDateTime(s.startsAt)
     ),
     chat option views.html.chat.frag