lichess-org/lila

allow swiss team leaders to moderate swiss chats - closes #7318

app/views/swiss/show.scala
@@ -16,7 +16,8 @@ object show {
   def apply(
       s: Swiss,
       data: play.api.libs.json.JsObject,
-      chatOption: Option[lila.chat.UserChat.Mine]
+      chatOption: Option[lila.chat.UserChat.Mine],
+      isLocalMod: Boolean
   )(implicit ctx: Context): Frag = {
     val isDirector       = ctx.userId.has(s.createdBy)
     val hasScheduleInput = isDirector && s.settings.manualRounds && s.isNotFinished
@@ -37,7 +38,8 @@ object show {
                   name = trans.chatRoom.txt(),
                   timeout = c.timeout,
                   public = true,
-                  resourceId = lila.chat.Chat.ResourceId(s"swiss/${c.chat.id}")
+                  resourceId = lila.chat.Chat.ResourceId(s"swiss/${c.chat.id}"),
+                  localMod = isLocalMod
                 )
               }
             )

RoepStoep/lidraughts

app/views/swiss/show.scala
@@ -16,7 +16,8 @@ object show {
     s: Swiss,
     verdicts: SwissCondition.All.WithVerdicts,
     data: play.api.libs.json.JsObject,
-    chatOption: Option[lidraughts.chat.UserChat.Mine]
+    chatOption: Option[lidraughts.chat.UserChat.Mine],
+    isLocalMod: Boolean
   )(implicit ctx: Context): Frag = {
     val isDirector = ctx.userId.has(s.createdBy)
     val hasScheduleInput = isDirector && s.settings.manualRounds && s.isNotFinished
@@ -38,7 +39,8 @@ object show {
                     name = trans.chatRoom.txt(),
                     timeout = c.timeout,
                     public = true,
-                    resourceId = lidraughts.chat.Chat.ResourceId(s"swiss/${c.chat.id}")
+                    resourceId = lidraughts.chat.Chat.ResourceId(s"swiss/${c.chat.id}"),
+                    localMod = isLocalMod
                   )
                 }
               )