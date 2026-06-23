lichess-org/lila

add swiss tournament result to user activity feed - closes #7708

app/views/activity.scala
@@ -8,6 +8,7 @@ import lila.api.Context
 import lila.app.templating.Environment._
 import lila.app.ui.ScalatagsTemplate._
 import lila.user.User
+import lila.swiss.Swiss
 
 object activity {
 
@@ -33,6 +34,7 @@ object activity {
             a.simuls map renderSimuls(u),
             a.studies map renderStudies,
             a.tours map renderTours,
+            a.swisses map renderSwisses,
             a.teams map renderTeams,
             a.stream option renderStream(u),
             a.signup option renderSignup
@@ -245,7 +247,6 @@ object activity {
         trans.activity.competedInNbTournaments.pluralSame(tours.nb),
         subTag(
           tours.best.map { t =>
-            val link = a(href := routes.Tournament.show(t.tourId))(tournamentIdToName(t.tourId))
             div(
               cls := List(
                 "is-gold" -> (t.rank == 1),
@@ -258,7 +259,32 @@ object activity {
                 strong(t.rank),
                 t.rankRatio.percent,
                 t.nbGames,
-                link
+                a(href := routes.Tournament.show(t.tourId))(tournamentIdToName(t.tourId))
+              ),
+              br
+            )
+          }
+        )
+      )
+    )
+
+  private def renderSwisses(swisses: List[(Swiss.IdName, Int)])(implicit ctx: Context) =
+    entryTag(
+      iconTag("g"),
+      div(
+        trans.activity.competedInNbSwissTournaments.pluralSame(swisses.size),
+        subTag(
+          swisses.map { case (swiss, rank) =>
+            div(
+              cls := List(
+                "is-gold" -> (rank == 1),
+                "text"    -> (rank <= 3)
+              ),
+              dataIcon := (rank <= 3).option("g")
+            )(
+              trans.activity.rankedInSwissTournament(
+                strong(rank),
+                a(href := routes.Swiss.show(swiss.id.value))(swiss.name)
               ),
               br
             )

modules/activity/src/main/BSONHandlers.scala
@@ -7,8 +7,10 @@ import lila.common.{ Day, Iso }
 import lila.db.dsl._
 import lila.rating.BSONHandlers.perfTypeKeyIso
 import lila.rating.PerfType
-import lila.study.BSONHandlers._
+import lila.study.BSONHandlers.StudyIdBSONHandler
 import lila.study.Study
+import lila.swiss.BsonHandlers.swissIdHandler
+import lila.swiss.Swiss
 import lila.user.User
 
 private object BSONHandlers {
@@ -117,6 +119,13 @@ private object BSONHandlers {
   implicit private lazy val teamsHandler =
     isoHandler[Teams, List[String]]((s: Teams) => s.value, Teams.apply _)
 
+  implicit lazy val swissRankHandler = new lila.db.BSON[SwissRank] {
+    def reads(r: lila.db.BSON.Reader)                = SwissRank(Swiss.Id(r.str("i")), r.intD("r"))
+    def writes(w: lila.db.BSON.Writer, s: SwissRank) = BSONDocument("i" -> s.id, "r" -> s.rank)
+  }
+  implicit private lazy val swissesHandler =
+    isoHandler[Swisses, List[SwissRank]]((s: Swisses) => s.value, Swisses.apply _)
+
   object ActivityFields {
     val id       = "_id"
     val games    = "g"
@@ -131,6 +140,7 @@ private object BSONHandlers {
     val follows  = "f"
     val studies  = "t"
     val teams    = "e"
+    val swisses  = "w"
     val stream   = "st"
   }
 
@@ -153,6 +163,7 @@ private object BSONHandlers {
         follows = r.getO[Follows](follows).filterNot(_.isEmpty),
         studies = r.getO[Studies](studies),
         teams = r.getO[Teams](teams),
+        swisses = r.getO[Swisses](swisses),
         stream = r.getD[Boolean](stream)
       )
 
@@ -171,6 +182,7 @@ private object BSONHandlers {
         follows  -> o.follows,
         studies  -> o.studies,
         teams    -> o.teams,
+        swisses  -> o.swisses,
         stream   -> o.stream.option(true)
       )
   }

RoepStoep/lidraughts

app/views/activity.scala
@@ -6,6 +6,7 @@ import lidraughts.api.Context
 import lidraughts.app.templating.Environment._
 import lidraughts.app.ui.ScalatagsTemplate._
 import lidraughts.user.User
+import lidraughts.swiss.Swiss
 
 import controllers.routes
 
@@ -34,6 +35,7 @@ object activity {
             a.simuls map renderSimuls(u),
             a.studies map renderStudies,
             a.tours map renderTours,
+            a.swisses map renderSwisses,
             a.teams map renderTeams,
             a.stream option renderStream(u),
             a.signup option renderSignup
@@ -250,7 +252,6 @@ object activity {
         trans.activity.competedInNbTournaments.pluralSame(tours.nb),
         subTag(
           tours.best.map { t =>
-            val link = a(href := routes.Tournament.show(t.tourId))(tournamentIdToName(t.tourId))
             div(
               cls := List(
                 "is-gold" -> (t.rank == 1),
@@ -258,9 +259,41 @@ object activity {
               ),
               dataIcon := (t.rank <= 3).option("g")
             )(
-                trans.activity.rankedInTournament.plural(t.nbGames, strong(t.rank), (t.rankRatio.value * 100).toInt atLeast 1, t.nbGames, link),
+                trans.activity.rankedInTournament.plural(
+                  t.nbGames,
+                  strong(t.rank),
+                  (t.rankRatio.value * 100).toInt atLeast 1,
+                  t.nbGames,
+                  a(href := routes.Tournament.show(t.tourId))(tournamentIdToName(t.tourId))
+                ),
                 br
               )
+          }
+        )
+      )
+    )
+
+  private def renderSwisses(swisses: List[(Swiss.IdName, Int)])(implicit ctx: Context) =
+    entryTag(
+      iconTag("g"),
+      div(
+        trans.activity.competedInNbSwissTournaments.pluralSame(swisses.size),
+        subTag(
+          swisses.map {
+            case (swiss, rank) =>
+              div(
+                cls := List(
+                  "is-gold" -> (rank == 1),
+                  "text" -> (rank <= 3)
+                ),
+                dataIcon := (rank <= 3).option("g")
+              )(
+                  trans.activity.rankedInSwissTournament(
+                    strong(rank),
+                    a(href := routes.Swiss.show(swiss.id.value))(swiss.name)
+                  ),
+                  br
+                )
           }
         )
       )

modules/activity/src/main/BSONHandlers.scala
@@ -8,6 +8,8 @@ import lidraughts.db.dsl._
 import lidraughts.rating.BSONHandlers.perfTypeKeyIso
 import lidraughts.rating.PerfType
 import lidraughts.study.Study
+import lidraughts.swiss.BsonHandlers.swissIdHandler
+import lidraughts.swiss.Swiss
 import lidraughts.user.User
 
 private object BSONHandlers {
@@ -103,6 +105,13 @@ private object BSONHandlers {
   private implicit val studiesHandler = isoHandler[Studies, List[Study.Id], Barr]((s: Studies) => s.value, Studies.apply _)
   private implicit val teamsHandler = isoHandler[Teams, List[String], Barr]((s: Teams) => s.value, Teams.apply _)
 
+  implicit lazy val swissRankHandler = new lidraughts.db.BSON[SwissRank] {
+    def reads(r: lidraughts.db.BSON.Reader) = SwissRank(Swiss.Id(r.str("i")), r.intD("r"))
+    def writes(w: lidraughts.db.BSON.Writer, s: SwissRank) = BSONDocument("i" -> s.id, "r" -> s.rank)
+  }
+  implicit private lazy val swissesHandler =
+    isoHandler[Swisses, List[SwissRank], Barr]((s: Swisses) => s.value, Swisses.apply _)
+
   object ActivityFields {
     val id = "_id"
     val games = "g"
@@ -118,6 +127,7 @@ private object BSONHandlers {
     val follows = "f"
     val studies = "t"
     val teams = "e"
+    val swisses = "w"
     val stream = "st"
   }
 
@@ -140,6 +150,7 @@ private object BSONHandlers {
       follows = r.getO[Follows](follows).filterNot(_.isEmpty),
       studies = r.getO[Studies](studies),
       teams = r.getO[Teams](teams),
+      swisses = r.getO[Swisses](swisses),
       stream = r.getD[Boolean](stream)
     )
 
@@ -158,6 +169,7 @@ private object BSONHandlers {
       follows -> o.follows,
       studies -> o.studies,
       teams -> o.teams,
+      swisses -> o.swisses,
       stream -> o.stream.option(true)
     )
   }